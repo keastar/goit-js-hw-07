a949d73a70aca7f1935d2274e31261b21794a5e4
keastar <keastar1986@gmail.com>
2023-02-13 16:58:43 +0200

    1 create stroke element Item
    1 create stroke element Item

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createImageItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+        })
+        .join('');
+   }
+        console.log(createImageItemsMarkup(galleryItems));
+
+// console.log(markup);
